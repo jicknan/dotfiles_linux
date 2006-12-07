0b07a0befb726e1565aebfe4d0fac178ae54cdc3
jick.nan <jick.nan@b9793199-251c-0410-9466-17b2cf80d641>
2006-12-07 09:41:36 +0000

    Test limit rate with libcurl.
    git-svn-id: https://jicknan.googlecode.com/svn/trunk@27 b9793199-251c-0410-9466-17b2cf80d641

--- doc/code/curl.c
+++ doc/code/curl.c
@@ -2,42 +2,52 @@
  * curl.c - test the usage of libcurl.
  *
  * compile:
- * 	gcc -o curl -lcurl curl.c
- *
- * Created at:  Wed 06 Dec 2006 08:03:38 PM UTC
- *
- * $Id: c.skel,v 1.3 2006/06/01 10:02:05 jick Exp $
+ *	gcc -o curl -lcurl curl.c
  */
 
 #include <stdio.h>
-#include <curl/curl.h>
 #include <string.h>
+#include <curl/curl.h>
+
+#define MAX_RATE	102400 // 100K
+FILE *fp = NULL;
+
+size_t write_data(void *ptr, size_t size, size_t nmemb, void *stream)
+{
+	if (!fp) {
+		fp = fopen("/dev/null", "w");
+		if (!fp) {
+			fprintf(stderr, "Fail to open file!\n");
+			return -1;
+		}
+	}
+	int written = fwrite(ptr, size, nmemb, (FILE *)fp);
+
+	return written;
+}
 
 int main(int argc, char *argv[])
 {
 	CURL *curl;
 	CURLcode res;
-	FILE *fp = NULL;
 
 	if (argc != 2) {
 		printf("USAGE: %s <url>\n", argv[0]);
 		return -1;
 	}
 
-	printf("%s\n", curl_version());
 	curl = curl_easy_init();
 	if (curl == NULL) {
 		printf("Error init libcurl!\n");
 		return -1;
 	}
 
-	fp = fopen("/dev/null", "w");
 	curl_easy_setopt(curl, CURLOPT_URL, argv[1]);
-	//curl_easy_setopt(curl, CURLOPT_ERRORBUFFER, errbuf);
+	curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, write_data);
 	curl_easy_setopt(curl, CURLOPT_WRITEDATA, fp);
-	curl_easy_setopt(curl, CURLOPT_BUFFERSIZE, 10240);
-	//curl_easy_setopt(curl, CURLOPT_MAX_RECV_SPEED_LARGE, 10240);
-	//curl_easy_setopt(curl, CURLOPT_MAX_RECV_PER_SECOND, 10240);
+	curl_easy_setopt(curl, CURLOPT_BUFFERSIZE, MAX_RATE);
+	curl_easy_setopt(curl, CURLOPT_MAX_RECV_SPEED_LARGE, MAX_RATE);
+	curl_easy_setopt(curl, CURLOPT_MAX_SEND_SPEED_LARGE, MAX_RATE);
 	//curl_easy_setopt(curl, CURLOPT_VERBOSE, 1);
 	res = curl_easy_perform(curl);
 	curl_easy_cleanup(curl);
