f5f95472f251f6cc456436f3c67e6c59270316ca
jick.nan <jick.nan@b9793199-251c-0410-9466-17b2cf80d641>
2006-12-08 07:23:04 +0000

    Test the old method for limit rate with libcurl, which was introduced in 7.10(about) at 2002.
    r27 is for the NEW method to limit rate, which was introduced in 7.15.5
    at 2006-06, but it's found not work in 7.16. The core curl developer
    (Daniel Stenberg) is testing for this bugs just now.
    
    
    
    git-svn-id: https://jicknan.googlecode.com/svn/trunk@29 b9793199-251c-0410-9466-17b2cf80d641

--- doc/code/curl.c
+++ doc/code/curl.c
@@ -7,13 +7,82 @@
 
 #include <stdio.h>
 #include <string.h>
+#include <sys/time.h>
 #include <curl/curl.h>
 
-#define MAX_RATE	102400 // 100K
-FILE *fp = NULL;
+#define MAX_RATE	204800 // 100K
+#define CURL_MAX_WRITE_SIZE 16384
 
-size_t write_data(void *ptr, size_t size, size_t nmemb, void *stream)
+FILE *fp = NULL;
+struct timeval lastrecvtime;
+static long recvpersecond = MAX_RATE;
+
+/* Return the current time in a timeval struct */
+struct timeval tvnow(void)
 {
+  struct timeval now;
+  (void)gettimeofday(&now, NULL);
+  return now;
+}
+
+/*
+ * Make sure that the first argument is the more recent time, as otherwise
+ * we'll get a weird negative time-diff back...
+ *
+ * Returns: the time difference in number of milliseconds.
+ */
+long tvdiff(struct timeval newer, struct timeval older)
+{
+  return (newer.tv_sec-older.tv_sec)*1000+
+    (newer.tv_usec-older.tv_usec)/1000;
+}
+
+static void go_sleep(long ms)
+{
+	struct timeval timeout;
+
+	timeout.tv_sec = ms/1000;
+	ms = ms%1000;
+	timeout.tv_usec = ms * 1000;
+
+	select(0, NULL, NULL, NULL, &timeout);
+}
+
+size_t write_data(void *ptr, size_t sz, size_t nmemb, void *stream)
+{
+
+	off_t size = (off_t)(sz * nmemb);
+
+	if(recvpersecond) {
+		struct timeval now;
+		long timediff;
+		long sleep_time;
+
+		static off_t addit = 0;
+
+		now = tvnow();
+		timediff = tvdiff(now, lastrecvtime); /* ms */
+
+		if((recvpersecond > CURL_MAX_WRITE_SIZE) && (timediff < 100) ) {
+			addit += size;
+		}
+		else {
+			size += addit;
+			addit = 0;
+
+			if (size*1000 > recvpersecond*timediff) {
+				sleep_time = (long)(size*1000/recvpersecond - timediff);
+				fprintf(stderr, "sleep_time=%ld\n", sleep_time);
+
+				if (sleep_time > 0) {
+					go_sleep(sleep_time);
+					now = tvnow();
+				}
+			}
+			lastrecvtime = now;
+		}
+	}
+
 	if (!fp) {
 		fp = fopen("/dev/null", "w");
 		if (!fp) {
@@ -21,7 +90,8 @@ size_t write_data(void *ptr, size_t size, size_t nmemb, void *stream)
 			return -1;
 		}
 	}
-	int written = fwrite(ptr, size, nmemb, (FILE *)fp);
+	//fprintf(stderr, "#");
+	int written = fwrite(ptr, sz, nmemb, (FILE *)fp);
 
 	return written;
 }
@@ -36,12 +106,14 @@ int main(int argc, char *argv[])
 		return -1;
 	}
 
+	printf("%s\n", curl_version());
 	curl = curl_easy_init();
 	if (curl == NULL) {
 		printf("Error init libcurl!\n");
 		return -1;
 	}
 
+	lastrecvtime = tvnow();
 	curl_easy_setopt(curl, CURLOPT_URL, argv[1]);
 	curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, write_data);
 	curl_easy_setopt(curl, CURLOPT_WRITEDATA, fp);
