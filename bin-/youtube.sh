19e27f6b139d3a1255609c093667f9690672b834
jick.nan <jick.nan@b9793199-251c-0410-9466-17b2cf80d641>
2007-05-27 13:22:21 +0000

    Move bin- to bin.
    git-svn-id: https://jicknan.googlecode.com/svn/trunk@117 b9793199-251c-0410-9466-17b2cf80d641

--- bin-/youtube.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-BASEURL="http://youtube.com/get_video.php?" ; read -p "YouTube URL? " ORIGURL ; read -p "Desired path/filename? " OUTFILE
-
-wget -c -S -O ${OUTFILE}.flv ${BASEURL}`curl -s ${ORIGURL} | grep player2.swf | cut -f2 -d? | cut -f1 -d\"` && ffmpeg -i ${OUTFILE}.flv -ab 56 -ar 22050 -b 500 -s 320x240 ${OUTFILE}.mpg && exit 0
