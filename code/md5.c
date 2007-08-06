b82b7316c75779476704652cbd91352b9216a1e1
jick.nan <jick.nan@b9793199-251c-0410-9466-17b2cf80d641>
2007-08-06 02:54:54 +0000

    Another call method.
    git-svn-id: https://jicknan.googlecode.com/svn/trunk@153 b9793199-251c-0410-9466-17b2cf80d641

--- code/md5.c
+++ code/md5.c
@@ -8,15 +8,36 @@
 #include <string.h>
 #include <openssl/md5.h>
 
+char *md5(char *in)
+{
+    MD5_CTX x;
+    int i;
+    char *out = NULL;
+    unsigned char d[16];
+
+    MD5_Init(&x);
+    MD5_Update (&x, in, strlen(in));
+    MD5_Final(d, &x);
+
+    out = malloc(35);
+    memset(out, 0x00, 35);
+    strcpy(out, "$1$");
+    for (i = 0; i < 16; i++) {
+	sprintf (out + (i*2), "%02x", d[i]);
+    }
+
+    return out;
+}
+
 int main(int argc, char *argv[])
 {
     char password[68] = {0};
-    MD5_CTX x;
     int i = 0, len;
-    char *out = NULL;
-    unsigned char d[16];
-    unsigned char tmp[128] = { 0 };
+    char *text = "abc";
 
+    printf("TEXT: %s\nMD5ed: %s\n", text, md5(text));
+
+#if 0
     if (argc != 2) {
 	printf("Usage: %s <message>(16~64)\n", argv[0]);
 	return -1;
@@ -29,30 +50,15 @@ int main(int argc, char *argv[])
 	return -1;
     }
 
+    // padding
     if (len < 64) {
 	for (i = 64/len; i >= 0; i--) {
 	    strcat(tmp, password);
 	}
 	strncpy(password, tmp, 64);
     }
+#endif
 
-    MD5_Init(&x);
-    MD5_Update (&x, (char *) password, strlen(password));
-    MD5_Final(d, &x);
-    out = malloc(35);
-    memset(out, 0x00, 35);
-    strcpy(out, "$1$");
-    printf ("MD5(\"%s\") = ", password);
-    for (i = 0; i < 16; i++) {
-	sprintf (out + (i*2), "%02X", d[i]);
-    }
-    printf ("%s(%d)\n", out, strlen(out));
-
-    printf ("MD5(\"%s\") = ", password);
-    for (i = 0; i < 16; i++) {
-	printf ("%02X", d[i]);
-    }
-    printf ("(%d)\n", strlen(d));
 
     return 0;
 }
