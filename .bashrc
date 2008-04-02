e67eacded674e675d96acba347f11544fd78601d
jick.nan <jick.nan@b9793199-251c-0410-9466-17b2cf80d641>
2008-04-02 16:03:48 +0000

    Update it for the new Archlinux.
    git-svn-id: https://jicknan.googlecode.com/svn/trunk@182 b9793199-251c-0410-9466-17b2cf80d641

--- .bashrc
+++ .bashrc
@@ -1,2 +1,7 @@
 #~/.bashrc
 [ -f /etc/profile ] && . /etc/profile
+
+alias ls='ls --color=auto'
+alias ll='ls -l'
+alias la='ls -a'
+PS1='[\u@\h \W]\$ '
---
+++ .config/openbox/autostart.sh
@@ -0,0 +1,23 @@
+# This shell script is run before Openbox launches.
+# Environment variables set here are passed to the Openbox session.
+
+# Set a background color
+#BG=xsetroot
+#test -z $BG || $BG -solid "#303030"
+
+# D-bus
+if which dbus-launch >/dev/null && test -z "$DBUS_SESSION_BUS_ADDRESS"; then
+       eval `dbus-launch --sh-syntax --exit-with-session`
+fi
+
+# Make GTK apps look and behave how they were set up in the gnome config tools
+if which gnome-settings-daemon >/dev/null; then
+  gnome-settings-daemon &
+fi
+
+# Preload stuff for KDE apps
+if which start_kdeinit >/dev/null; then
+  LD_BIND_NOW=true start_kdeinit --new-startup +kcminit_startup &
+fi
+
+# Programs that will run after Openbox has started
