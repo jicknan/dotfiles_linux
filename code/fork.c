7182e56c9f0306f51cb5775ad43c76f5d5e25a26
Jick Nan <jick.nan@gmail.com>
2008-06-12 11:07:24 +0800

    Update it

--- code/fork.c
+++ code/fork.c
@@ -19,13 +19,17 @@ int main(void)
 	var = 88;
 	printf("before fork\n");
 
-	if ( (pid = vfork()) < 0) {
+	//if ( (pid = fork()) < 0) {
+	if ( (pid = fork()) < 0) {
 		printf("fork error\n");
 		return -1;
 	} else if (pid == 0) { /* child */
 		glob ++;
 		var ++;
+	printf("pid = %d, glob = %d, var = %d\n", getpid(), glob, var);
 		_exit(0);
+	} else {
+		sleep(2);
 	}
 
 	// parent
