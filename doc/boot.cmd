5eed7cc7becdcb74d69fa2e56d63c8c92bae1bf7
Jick Nan <jick.nan@gmail.com>
2008-08-27 16:35:51 +0800

    Update it.

--- doc/boot.cmd
+++ doc/boot.cmd
@@ -2,7 +2,7 @@ u-boot for NS65:
 ================
 set ethaddr 00:c0:02:00:c0:66; set bootargs console=ttyS0,115200 root=/dev/ram; set serverip 172.21.6.12; set ipaddr 172.21.6.77; tftpboot 0x400000 uImage; tftpboot 0x1100000 romdisk; bootm 0x400000
 
-set bootargs console=ttyS0,115200 root=/dev/ram; set ethaddr 00:c0:02:00:c0:66; set serverip 172.21.6.70; set ipaddr 172.21.6.77; tftpboot 0x400000 uImage; tftpboot 0x1100000 romdisk; bootm 0x400000
+set ethaddr 00:c0:02:00:c0:66; set bootargs console=ttyS0,115200 root=/dev/ram; set serverip 172.21.6.133; set ipaddr 172.21.6.77; tftpboot 0x400000 uImage; tftpboot 0x2c00000 ramdisk.gz; bootm 0x400000
 
 set ethaddr 00:c0:02:00:c0:66; set serverip 172.21.7.20; set ipaddr 172.21.6.77; tftpboot 0x400000 uImage;tftpboot 0x1100000 romdisk; bootm 0x400000
 
@@ -12,9 +12,7 @@ set ethaddr 00:c0:02:00:c0:66; set serverip 172.21.6.7; set ipaddr 172.21.6.77; 
 
 set bootargs console=ttyS0,115200 root=/dev/ram; set ethaddr 00:c0:02:00:c0:66; set serverip 192.168.0.10; set ipaddr 192.168.0.11; tftpboot 0x400000 uImage; tftpboot 0x1100000 romdisk; bootm 0x400000
 
-u-boot for foonas
-=================
-set bootargs console=ttyS0,115200 root=/dev/ram0; set serverip 172.21.6.17; set ipaddr 172.21.6.77; tftpboot 0x400000 uImage_2.6.24; bootm 0x400000
+set bootargs console=ttyS0,115200 root=/dev/ram0; set serverip 172.21.6.133; set ipaddr 172.21.6.77; tftpboot 0x400000 uImage-2.6.26.2-r0-ts209-initramfs.bin; bootm 0x400000
 
 RedBoot for NS54:
 =================
